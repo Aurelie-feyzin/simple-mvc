678f010254b9a5dbd5427d3ac62deadaa961219b
Aurelie <gramusset.a@gmail.com>
2018-10-05 16:44:09 +0200

    MVC quete 4 tempalte Twig

--- public/index.php
+++ public/index.php
@@ -2,25 +2,3 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/dispatcher.php';
-
-/* A Simple router
-list url :
-http://localhost:8000/index.php?route=items
-http://localhost:8000/index.php?route=item&id=$id
-
-
-$route = $_GET['route'] ?? '';
-
-if ($route === 'items') {
-    $itemController = new Controller\ItemController();
-    $items = $itemController->index();
-} elseif ($route == 'item') {
-    $itemController = new Controller\ItemController();
-    $item = $itemController->show($_GET['id']);
-}else {
-    header("HTTP/1.0 404 Not Found");
-    echo 'Page introuvable';
-}
-
-*/
-
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,26 +3,38 @@
 namespace Controller;
 
 use Model\CategoryManager;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 
 class CategoryController
 {
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem( __DIR__ . '/../View');
+        $this->twig = new Twig_Environment($loader);
+    }
+
     public function index()
     {
         $categoryManager = new CategoryManager();
         $categories = $categoryManager->selectAllCategories();
-        require __DIR__ . '/../View/Category/index.php';
 
-        return $categories;
+        echo $this->twig->render('Category/index.html.twig', [
+            'categories' => $categories,
+        ]);
     }
 
     public function show($id)
     {
         $categoryManager = new CategoryManager();
         $category = $categoryManager->selectOneCategory($id);
-        require __DIR__ . '/../View/Category/show.php';
 
-        return $category;
+        echo $this->twig->render('Category/show.html.twig', [
+            'category' => $category,
+        ]);
     }
 
 }
